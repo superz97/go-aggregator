1dfe63fd69e5bf7cb3a9a29a1006c9eb683abd14
zbarojan <zbarojan@innoforce.kz>
2026-06-04 12:02:16 +0500

    ch2:database; l3:sqlc; ref user handler

--- handler_user.go
+++ handler_user.go
@@ -9,6 +9,22 @@ import (
 	"github.com/google/uuid"
 )
 
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("login requires a username argument")
+	}
+
+	name := cmd.args[0]
+	_, err := s.db.GetUser(context.Background(), name)
+	if err != nil {
+		return fmt.Errorf("user %s does not exist", name)
+	}
+
+	s.cfg.SetUser(name)
+	fmt.Printf("User has been set to: %s\n", name)
+	return nil
+}
+
 func handlerRegister(s *state, cmd command) error {
 	if len(cmd.args) == 0 {
 		return fmt.Errorf("register requires at least one argument")
