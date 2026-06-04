3549a1c6cc3b5516ddf72238b15a2215a214b677
zbarojan <zbarojan@innoforce.kz>
2026-06-04 14:39:57 +0500

    ch3:rss; l1:rss;

--- main.go
+++ main.go
@@ -36,6 +36,7 @@ func main() {
 	cmds.register("register", handlerRegister)
 	cmds.register("reset", handlerReset)
 	cmds.register("users", handlerUsers)
+	cmds.register("agg", handlerAgg)
 
 	args := os.Args
 	if len(args) < 2 {
